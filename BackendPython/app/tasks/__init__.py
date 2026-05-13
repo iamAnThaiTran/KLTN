6fea63acde158fbb9da61f507a8d5d5df5d6674b
anthaitran <anthaitran1001@gmail.com>
2026-05-14 00:15:38 +0700

    delete backend python

--- BackendPython/app/tasks/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/tasks/__init__.py
-"""
-Tasks module - Background job processing
-Handles async task execution via RabbitMQ
-"""
-
-from .crawl_worker import CrawlWorker
-
-__all__ = ["CrawlWorker"]
